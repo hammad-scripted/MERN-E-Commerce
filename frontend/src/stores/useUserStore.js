438b7cee43c3ce93429e862a495d05b458d66b46
Mohammad Hammad <hammad.scripted@gmail.com>
2026-08-02 00:13:29 +0530

    added axios interceptors

--- frontend/src/stores/useUserStore.js
+++ frontend/src/stores/useUserStore.js
@@ -2,6 +2,7 @@ import { create } from 'zustand';
 import axiosInstance from '../lib/axios';
 import { toast } from 'react-hot-toast';
 import { getErrorMessage } from '../lib/getErrorMessage';
+
 export const useUserStore = create((set, get) => ({
   // ? states
   user: null,
@@ -62,5 +63,38 @@ export const useUserStore = create((set, get) => ({
       toast.error(getErrorMessage(error));
     }
   },
-  //todo implement the axios interceptors for refreshing
 }));
+
+let refreshingToken = null;
+
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+
+    if (!error.response || error.response.status !== 401 || originalRequest._retry) {
+      return Promise.reject(error);
+    }
+
+    originalRequest._retry = true;
+
+    try {
+      if (!refreshingToken) {
+        refreshingToken = axiosInstance.post('auth/refresh-token');
+      }
+
+      const res = await refreshingToken;
+      const accessToken = res.data.data.accessToken;
+
+      axiosInstance.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`;
+      originalRequest.headers.Authorization = `Bearer ${accessToken}`;
+
+      return axiosInstance(originalRequest);
+    } catch (refreshError) {
+      console.log(refreshError);
+      return Promise.reject(error);
+    } finally {
+      refreshingToken = null;
+    }
+  }
+);
